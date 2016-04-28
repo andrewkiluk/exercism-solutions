a79444b70ab5dc099332ff4b452ee6cd5653d9d5
Andrew Kiluk <andrew.kiluk@gmail.com>
2016-04-28 08:15:28 -0700

    Add Scrabble Score

---
+++ haskell/scrabble-score/Scrabble.hs
@@ -0,0 +1,40 @@
+module Scrabble where
+
+import Data.Maybe
+import Data.Char
+import Data.Map(Map)
+import qualified Data.Map as Map
+
+scores = Map.fromList [
+        ('A', 1),
+        ('B', 3),
+        ('C', 3),
+        ('D', 2),
+        ('E', 1),
+        ('F', 4),
+        ('G', 2),
+        ('H', 4),
+        ('I', 1),
+        ('J', 8),
+        ('K', 5),
+        ('L', 1),
+        ('M', 3),
+        ('N', 1),
+        ('O', 1),
+        ('P', 3),
+        ('Q', 10),
+        ('R', 1),
+        ('S', 1),
+        ('T', 1),
+        ('U', 1),
+        ('V', 4),
+        ('W', 4),
+        ('X', 8),
+        ('Y', 4),
+        ('Z', 10) ]
+
+scoreLetter :: Char -> Int
+scoreLetter letter = fromMaybe 0 $ Map.lookup (toUpper letter) scores
+
+scoreWord = sum . (map scoreLetter)
+
