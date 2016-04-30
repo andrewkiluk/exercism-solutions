00ce409ff685ceac74af24e525d1d0bfc123704b
Andrew Kiluk <andrew.kiluk@gmail.com>
2016-04-29 18:07:17 -0700

    Improve Roman numeral solution

--- haskell/roman-numerals/Roman.hs
+++ haskell/roman-numerals/Roman.hs
@@ -6,37 +6,18 @@ numerals x | x >= 100 = (calculateHundreds x) ++ numerals (x `mod` 100)
 numerals x | x >= 10 = (calculateTens x) ++ numerals (x `mod` 10)
 numerals x = calculateOnes x
 
-calculateThousands x = if x >= 1000 then "M" ++ calculateThousands (x - 1000) else ""
+calculateThousands = calculate 1000 "M" "" ""
+calculateHundreds = calculate 100 "C" "D" "M"
+calculateTens = calculate 10 "X" "L" "C"
+calculateOnes = calculate 1 "I" "V" "X"
 
-calculateHundreds x | x >= 900  = "CM"
-                    | x >= 800  = "DCCC"
-                    | x >= 700  = "DCC"
-                    | x >= 600  = "DC"
-                    | x >= 500  = "D"
-                    | x >= 400  = "CD"
-                    | x >= 300  = "CCC"
-                    | x >= 200  = "CC"
-                    | x >= 100  = "C"
-                    | otherwise = ""
-
-calculateTens x | x >= 90   = "XC"
-                | x >= 80   = "LXXX"
-                | x >= 70   = "LXX"
-                | x >= 60   = "LX"
-                | x >= 50   = "L"
-                | x >= 40   = "XL"
-                | x >= 30   = "XXX"
-                | x >= 20   = "XX"
-                | x >= 10   = "X"
-                | otherwise = ""
-
-calculateOnes x | x >= 9    = "IX"
-                | x >= 8    = "VIII"
-                | x >= 7    = "VII"
-                | x >= 6    = "VI"
-                | x >= 5    = "V"
-                | x >= 4    = "IV"
-                | x >= 3    = "III"
-                | x >= 2    = "II"
-                | x >= 1    = "I"
-                | otherwise = ""
+calculate magnitude low med high x | x >= 9*magnitude = low ++ high
+                                   | x >= 8*magnitude = med ++ low ++ low ++ low
+                                   | x >= 7*magnitude = med ++ low ++ low
+                                   | x >= 6*magnitude = med ++ low
+                                   | x >= 5*magnitude = med
+                                   | x >= 4*magnitude = low ++ med
+                                   | x >= 3*magnitude = low ++ low ++ low
+                                   | x >= 2*magnitude = low ++ low
+                                   | x >= 1*magnitude = low
+                                   | otherwise = ""
