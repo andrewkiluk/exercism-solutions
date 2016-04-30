361e96c30c9cf65fff652926471619ebae1bc022
Andrew Kiluk <andrew.kiluk@gmail.com>
2016-04-30 09:51:19 -0700

    Add Crypto Square

---
+++ haskell/crypto-square/CryptoSquare.hs
@@ -0,0 +1,29 @@
+module CryptoSquare (normalizePlaintext,
+                     squareSize,
+                     plaintextSegments,
+                     ciphertext,
+                     normalizeCiphertext) where
+
+import Data.Char
+import Data.List
+import Data.List.Split
+
+squareSize :: String -> Int
+squareSize text = head $ dropWhile (squareTooSmall text) [1,2..]
+    where squareTooSmall text n = length text > n^2
+
+normalizePlaintext :: String -> String
+normalizePlaintext = map toLower . filter isAlphaNum
+
+plaintextSegments :: String -> [String]
+plaintextSegments input = chunksOf (squareSize normalized) normalized
+    where normalized = normalizePlaintext input
+
+ciphertext :: String -> String
+ciphertext = concat . encipher
+
+normalizeCiphertext :: String -> String
+normalizeCiphertext input = concat . intersperse " " $ encipher input
+
+encipher :: String -> [String]
+encipher = transpose . plaintextSegments
